9c53419ca04c7ef87f58f2ac4c0b3427d91fda4d
awsandgit <aws4em@gmail.com>
2025-04-28 12:52:57 +0530

    Fixed subscript in names

--- build-site.el
+++ build-site.el
@@ -1,4 +1,5 @@
 (require 'package)
+(setq org-modify-urls t)
 (setq package-user-dir (expand-file-name "./.packages"))
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
